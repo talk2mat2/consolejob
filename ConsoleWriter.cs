1de9fac40b1b938e64cefebfbe43d06940217a95
Martins Chukuwuma <talk2mat2@yahoo.com>
2023-09-11 00:32:04 +0100

    initials

---
+++ ConsoleWriter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testconsoleapp
+{
+    internal class ConsoleWriter : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            while(!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("hello martins");
+                await Delay(432000000000);
+                
+            }
+          
+        }
+        static async Task Delay(long delay)
+        {
+            while (delay > 0)
+            {
+                var currentDelay = delay > int.MaxValue ? int.MaxValue : (int)delay;
+                await Task.Delay(currentDelay);
+                delay -= currentDelay;
+            }
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using testconsoleapp;
+
+Host.CreateDefaultBuilder().ConfigureServices
+    (services => services.AddHostedService<ConsoleWriter>())
+    .Build().Run();
